0416b37c9c5fd92c6efba162d7b325be0b166a0d
Rafa MZ <mz.tears.of.time@gmail.com>
2024-09-23 17:38:32 -0300

    17. Polimorfismo

--- 7-index.js
+++ 7-index.js
@@ -7,5 +7,5 @@ const novo_User = new User ( "Eli", "eli@.com", "10-10/1010" );
 console.log ( novo_User.Exibir_Info() );
 console.log ( `\n` );
 
-const novo_Admin = new Admin ( "Sophie", "sophie@.com", "20-10/1010")
+const novo_Admin = new Admin ( "Sophie", "sophie@.com", "20-10/1010" );
 console.log ( novo_Admin.Exibir_Info() );
