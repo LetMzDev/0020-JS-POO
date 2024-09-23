c5fcacffc823a2334a332470cc01eb594ba0961b
Rafa MZ <mz.tears.of.time@gmail.com>
2024-09-23 10:19:58 -0300

    9. Class

---
+++ 4-User.js
@@ -0,0 +1,26 @@
+class User
+{
+	constructor ( nome, email, nascimento, role, ativo = true )
+	{
+		this.nome = nome;
+		this.email = email;
+		this.nascimento = nascimento;
+		this.role = role || "estudante";
+		this.ativo = ativo;
+	}
+
+	Exibir_Info ()
+	{
+		return `${this.nome}, ${this.email}`;
+	}
+}
+
+const novo_User = new User ( "Sophie", "Sophie@.com", "15-11-3500" );
+
+console.log ( novo_User );
+console.log ( `\n` );
+console.log ( novo_User.Exibir_Info() );
+console.log ( `\n` );
+
+console.log( User.prototype.isPrototypeOf ( novo_User )); // User é protótipo de novo_User? true
+console.log ( `\n` );
